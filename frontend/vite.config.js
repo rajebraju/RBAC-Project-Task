0a0d68d64eff73fe470185f6451016af5fc822e6
Rajeb Chowdhury <rajebraju@gmail.com>
2025-08-24 04:00:08 +0600

    congif vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         // target: 'http://localhost:8080',
-        target: 'https://rbac-project-task-1.onrender.com',
+        target: process.env.REACT_APP_API_URL,
         changeOrigin: true,
         secure: false,
       }
